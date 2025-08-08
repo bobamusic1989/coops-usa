924bc3094bc45bd897312f8881e7d25199bdab3b
Alex Tarnowski <atrex92@gmail.com>
2025-08-08 12:56:10 -0500

    initial

---
+++ app/CoopLandingStandalone.jsx
@@ -0,0 +1,4 @@
+import React, { useMemo, useRef, useState, useEffect } from "react";
+export default function CoopLandingStandalone() {
+  return <div style={{color: 'white', background: 'black', height: '100vh', display:'flex', alignItems:'center', justifyContent:'center'}}>Coop Landing Placeholder - Paste full component here</div>;
+}
